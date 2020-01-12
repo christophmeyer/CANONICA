11aa4ab1af2983a2488d6bb395d235171cf6243a
Vladyslav Shtabovenko <dev.vsht@posteo.de>
2020-01-12 20:41:08 +0100

    Introduce a prototype of the test suite for running examples. Currently only for DrellYanOneMass.

--- examples/DrellYanOneMass/RunExample.m
+++ examples/DrellYanOneMass/RunExample.m
@@ -1,13 +1,66 @@
-Get["../../src/CANONICA.m"];
-Get["./DrellYanOneMassDEQ.m"];
+(* ::Package:: *)
+
+(* ::Title:: *)
+(*Drell-Yan with one internal mass*)
+
+
+(* ::Section:: *)
+(*Load CANONICA and the matrix for this example*)
+
+
+If[$FrontEnd===Null,
+	exDirectory=DirectoryName[$InputFileName];
+	verbosity=4;
+	colorPrint=Function[WriteString["stdout","\033[1m"<>#1<>"\033[0m "<> If[TrueQ[#4],
+		"\033[1m \033[32m"<>#2<>"\033[0m \033[0;39m","\033[1m \033[31m"<>#3<>"\033[0m \033[0;39m"]<>"\n"]],
+	
+	exDirectory=NotebookDirectory[];
+	verbosity=10;
+	colorPrint=Function[Print[Style[#1,"Text",Bold], " ", 
+		If[TrueQ[#4],Style[#2,"Text",Darker[Green],Bold],Style[#3,"Text",Red,Bold]]]];
+];
+caDirectory=ParentDirectory@ParentDirectory@exDirectory;
+caPath = FileNameJoin[{caDirectory,"src","CANONICA.m"}];
+
+
+Block[{Print=quiet},Get[caPath]];
+description="Drell-Yan with one internal mass";
+Get[FileNameJoin[{exDirectory,"DrellYanOneMassDEQ.m"}]];
+
+
+If[!MatchQ[aFull,{_?MatrixQ..}],
+	Print["Cannot load the matrix aFull, evaluation aborted!"];
+	If[$FrontEnd===Null,
+		Exit[-1],
+		Abort
+	]
+];
+
+
+(* ::Section:: *)
+(*Use CANONICA to find a canonical form*)
+
+
+Print[description];
+
 
 invariants = {x, y};
 sectorBoundaries = SectorBoundariesFromDE[aFull];
 
-fullResult = 
-RecursivelyTransformSectors[aFull, invariants, sectorBoundaries, {1, 20}];
 
-If[FileExistsQ["Result.m"], DeleteFile["Result.m"]];
-Save["Result.m", fullResult];
+maxMem=MaxMemoryUsed[fullResult = 
+	RecursivelyTransformSectors[aFull, invariants, 
+	sectorBoundaries, {1, 20},VerbosityLevel->verbosity];];
 
-Exit[];
+
+(* ::Section:: *)
+(*Check the final result*)
+
+
+colorPrint["Check the obtained epsilon form:","CORRECT","WRONG",
+CheckEpsForm[fullResult[[2]], invariants,ExtractIrreducibles[aFull]]];
+Print["\tCPU Time used: ", Round[N[TimeUsed[],4],0.001], " s."];
+Print["\tRAM used: ", Round[N[maxMem/10^6]], " MB"];
+
+
+
